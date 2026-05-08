5d2b80ad9439e40ef9ce4211eb1c218c1a58861a
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2026-05-08 11:27:26 +0200

    wip: home page prepared

--- config/node/frontend/src/pages/Home.js
+++ config/node/frontend/src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {Typography, Button} from "@mui/material";
+import {Link} from "react-router-dom";
+
+import './Home.css';
 
 function Home() {
 
-    return (
-        <div>
-            HOME
-        </div>
-    );
+    return (<div className="home">
+        <h1 className="home__title"> GEOPORTAL</h1>
+
+        <Typography className="home__subtitle">
+            Geoportal tematyczny poświęcony danym przestrzennym
+        </Typography>
+
+        <Button
+            className="home__button"
+            variant="contained"
+            size="large"
+            component={Link}
+            to='services'
+        >
+            START
+        </Button>
+
+    </div>);
 }
 
 
